3c4558165813c69c4586d18b1666db5f4aa91bfb
rousor <vw5yymst@gmail.com>
2018-10-06 08:54:22 +0000

    tamene

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,11 +20,11 @@
             </div>
         </div>
     @else
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Micropostsへようこそ！</h1>
-            {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
     @endif
 @endsection
